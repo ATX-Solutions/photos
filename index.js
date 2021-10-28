68c08dbf8fcede6537d864c6da850a9eb6e21244
Robert Parasca <parascarobert@gmail.com>
2021-10-28 23:32:15 +0300

    feat: initial commit

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const fs = require('fs');
-const axios = require('axios');
+const fs = require("fs");
+const axios = require("axios");
 
 const main = async () => {
+  console.log("test");
 
-	console.log('test');
+  try {
+    const { data } = await axios.get(
+      "https://api.pexels.com/v1/search?query=nature&per_page=100",
+      {
+        headers: {
+          Authorization:
+            "563492ad6f91700001000001d5c577a776b84794b80df25211170944",
+        },
+      }
+    );
 
-	try {
-		const { data } = await axios.get(
-			'https://api.pexels.com/v1/search?query=nature&per_page=100',
-			{
-				headers: {
-					Authorization: '563492ad6f91700001000001d5c577a776b84794b80df25211170944'
-				}
-			}
-		);
-		
-		// console.log(data);
+    // console.log(data);
 
-		fs.writeFileSync('data.json', JSON.stringify(data));
-
-	} catch (e) {
-		console.log(e);
-	}
-
+    fs.writeFileSync("data.json", JSON.stringify(data));
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 main();
